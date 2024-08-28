fc04f2ad70842051042c0663bef81fc02e9b3550
Elvin <harriselvin6@gmail.com>
2024-08-28 14:23:21 +0200

    added cors to index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import userRouter from './routes/usersRoute.js'
 import prodRouter from './routes/productsRoute.js'
 
 const port = process.env.PORT || 2000
 const app = express()
+
+app.use(cors({
+    origin: '*',
+    credentials: true
+}))
+
 
 app.use(express.static('public'))
 app.use(express.json())
