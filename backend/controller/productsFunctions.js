b1677386a1388fecf85ea770233eefaf5e6cbd0f
Elvin <harriselvin6@gmail.com>
2024-08-28 10:43:55 +0200

    added get routes that displays users and products

--- backend/controller/productsFunctions.js
+++ backend/controller/productsFunctions.js
@@ -1,7 +1,1 @@
-import { getProductsDB } from '../model/productsDB.js'
 
-const fetchProducts = async (req, res) => {
-  res.json(await getProductsDB())
-}
-
-export { fetchProducts }
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,8 +1,1 @@
-import express from 'express'
-import { fetchProducts } from '../controller/productsFunctions.js'
 
-const prodRouter = express.Router()
-
-prodRouter.get('/', fetchProducts)
-
-export default prodRouter
