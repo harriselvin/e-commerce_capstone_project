1ca60f1ea504284efe947cbb0eed49eb422ce732
harriselvin <harriselvin6@gmail.com>
2024-09-03 14:49:45 +0200

    added post, delete and patch routes for products and users

--- model/productsDB.js
+++ model/productsDB.js
@@ -25,5 +25,17 @@ const getProductDB = async (id) => {
   }
 };
 
+const addProductDB = async () => {
+  try {
+    let [data] = await pool.query(`
+      INSERT INTO products (prodName, prodDescription, prodPrice, prodImage)
+      VALUES (?, ?, ?, ?)
+      `, [prodName, prodDescription, prodPrice, prodImage]);
+      return data;
+    } catch (error) {
+      console.error('Error adding product:', error);
+      throw error;
+    }
+}
 
-export { getProductsDB, getProductDB }
+export { getProductsDB, getProductDB, addProductDB }
--- model/usersDB.js
+++ model/usersDB.js
@@ -16,7 +16,7 @@ const getUserDB = async (id) => {
   try {
     let [[data]] = await pool.query(`
       SELECT * FROM users
-      WHERE UserID = ?
+      WHERE userID = ?
     `, [id]);
     return data;
   } catch (error) {
@@ -25,4 +25,62 @@ const getUserDB = async (id) => {
   }
 };
 
-export { getUsersDB, getUserDB }
+const addUserDB = async (firstName, lastName, age, gender, country, userRole, email, password, userProfile) => {
+  try {
+    await pool.query(`
+      INSERT INTO users (firstName, lastName, age, gender, country, userRole, email, password, userProfile)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `, [firstName, lastName, age, gender, country, userRole, email, password, userProfile]);
+    } catch (error) {
+      console.error('Error adding user:', error);
+      throw error
+    }
+}
+
+const deleteUserDB = async (id) => {
+  try {
+    await pool.query(`
+      DELETE FROM users
+      WHERE userID = ?
+      `, [id])
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    throw error;
+  }
+}
+
+const updateUserDB = async (firstName, lastName, age, gender, country, userRole, email, password, userProfile, id) => {
+  try {
+    await pool.query(`
+      UPDATE users
+      SET firstName = ?,
+      lastName = ?,
+      age = ?,
+      gender = ?,
+      country = ?,
+      userRole = ?,
+      email = ?,
+      password = ?,
+      userProfile = ?
+      WHERE userID = ?
+      `, [firstName, lastName, age, gender, country, userRole, email, password, userProfile, id])
+  } catch (error) {
+    console.error('Error updating user:', error);
+    throw error
+  }
+}
+
+const loginUserDB = async (email) => {
+  try {
+    let [[data]] = await pool.query(`
+      SELECT * FROM users
+      WHERE email = ?
+      `, [email])
+    return data
+  } catch (error) {
+    console.error('Error logging in user:', error);
+    throw error
+  }
+}
+
+export { getUsersDB, getUserDB, addUserDB, deleteUserDB, updateUserDB, loginUserDB }
