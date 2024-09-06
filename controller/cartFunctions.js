6b3d826aa3861499fa3b60c2e652a322430a35dd
harriselvin <harriselvin6@gmail.com>
2024-09-06 14:19:54 +0200

    added the cart routes

--- controller/cartFunctions.js
+++ controller/cartFunctions.js
@@ -1,8 +1,11 @@
 import { getCartsDB, addToCartDB, deleteCartItemsDB, deleteCartDB, updateCartDB } from "../model/cartDB.js"; 
+import { pool } from '../model/cartDB.js'
 
 const fetchCartItems = async (req, res) => {
+    const userId = req.params.id
+
     try {
-        const cartItems = await getCartsDB();
+        const cartItems = await getCartsDB(userId);
         res.json(cartItems);
     } catch (error) {
         console.error(error);
@@ -10,28 +13,31 @@ const fetchCartItems = async (req, res) => {
     }
 }
 
-const fetchCartItem = async (req, res) => {
-    try {
-        const cartItem = await getCartDB(req.params.id);
-        if (!cartItem) {
-            res.status(404).send('Cart item not found');
-        } else {
-            res.json(cartItem);
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error fetching cart item');
-    }
-}
+const addToCart = async (req, res, userId) => {
+    const { prodID, quantity } = req.body
 
-const addToCart = async (req, res) => {
+    if (!prodID || !quantity) {
+        return res.status(400).send({ message: 'Invalid request data' });
+    }
+
     try {
-        const { userID, prodID } = req.body;
-        await addToCartDB(userID, prodID);
-        res.status(200).send('Cart item was successfully added');
+        // Check if the item already exists
+        const [existingItems] = await pool.query(`
+            SELECT * FROM cart WHERE userID = ? AND prodID = ?
+            `, [userId, prodID])
+
+        if (existingItems.length > 0) {
+            // If item exists, update the quantity
+            await updateCartDB(userId, prodID, quantity)
+        } else {
+            // If item does not exist, add it
+            await addToCartDB(userId, prodID, quantity);
+        }
+
+        res.status(201).send({ message: 'Item added to cart successfully' })
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error adding cart item');
+        console.error('Error adding item to cart', error);
+        res.status(500).send({ message: 'Error adding item to cart'});
     }
 }
 
@@ -57,23 +63,21 @@ const deleteCartItem = async (req, res) => {
     }
 }
 
-const updateCartItem = async (req, res) => {
+const updateCartItem = async (req, res, userID, prodID, quantity) => {
+    if (!userID || !prodID || !quantity) {
+        return res.status(400).send('Invalid request data');
+    }
+    
     try {
-        const cartItemID = req.params.id;
-        const { userID, prodID } = req.body;
-        const cartItem = await getCartDB(cartItemID);
-        if (!cartItem) {
-            res.status(404).send('Cart item not found');
-        } else {
-            const newUserID = userID || cartItem.userID;
-            const newProdID = prodID || cartItem.prodID;
-            await updateCartDB(newUserID, newProdID, cartItemID);
-            res.status(200).send('Cart info was successfully updated');
-        }
+        await updateCartDB(userID, prodID, quantity);
+        return res.status(200).send('Cart item quantity was successfully updated');
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error updating cart item');
+        console.error('Error updating cart item', error);
+
+        if (!res.headerSent) {
+            return res.status(500).send('Error updating cart item');
+        }
     }
 }
 
-export { fetchCartItems, fetchCartItem, addToCart, deleteAllCartItems, deleteCartItem, updateCartItem }
+export { fetchCartItems, addToCart, deleteAllCartItems, deleteCartItem, updateCartItem }
