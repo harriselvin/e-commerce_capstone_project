a50ac8c921e05cf16668c60fdb003d84b8364f10
harriselvin <harriselvin6@gmail.com>
2024-09-11 15:04:22 +0200

    initial commit

--- routes/adminUsersRoute.js
+++ routes/adminUsersRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { fetchAdminUsers, fetchAdminUser, addAdminUser, loginAdminUser, updateAdminUser, deleteAdminUser } from '../controller/adminUsersFunctions.js'
-import { checkUser, checkAdminUser } from '../middleware/authenticate.js';
-
+import { checkAdminUser } from '../middleware/authenticate.js';
+ 
 const adminUserRouter = express.Router()
 
 adminUserRouter.get('/admins', fetchAdminUsers)
